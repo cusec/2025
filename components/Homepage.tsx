1bd7981cac096411db19d3d898555b4012127752
TyrellHaywood <tyrelltman@gmail.com>
2024-09-09 22:06:42 -0400

    refactor: changed button animation delay w/ framer variants

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 import Image from "next/image";
 
 const transitionConfig = (delay = 0) => ({
@@ -10,15 +10,57 @@ const transitionConfig = (delay = 0) => ({
   delay: delay,
 });
 
+const rippleConfig = () => ({
+  type: "spring",
+  duration: 2,
+  repeat: Infinity,
+  RepeatType: "mirror",
+});
+
+const parentVariants: Variants = {
+  hidden: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.1,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const childVariants: Variants = {
+  hidden: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+      stiffness: 50,
+      duration: 1,
+    },
+  },
+};
+
 export default function Homepage() {
   return (
     <>
-      <motion.div className="flex flex-col items-center w-4/5 z-10 mt-[-150px]">
+      <motion.div
+        className="flex flex-col items-center w-4/5 z-10 mt-[-150px]"
+        variants={parentVariants}
+      >
         <motion.div
           className="flex justify-center mb-[80px]"
           transition={transitionConfig()}
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
+          variants={childVariants}
+          initial="hidden"
+          animate="visible"
         >
           <Image
             className=" w-[90%] max-w-[325px]"
@@ -31,44 +73,40 @@ export default function Homepage() {
 
         <motion.div
           id="text"
-          transition={transitionConfig(0.1)}
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
+          transition={transitionConfig(0)}
+          variants={parentVariants}
+          initial="hidden"
+          animate="visible"
         >
           <motion.h2
             className="text-center text-[14px] text-font"
-            transition={transitionConfig(0.2)}
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
+            variants={childVariants}
           >
             CANADIAN UNIVERSITY
           </motion.h2>
           <motion.h2
             className="text-center text-[14px] text-font"
-            transition={transitionConfig(0.3)}
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
+            variants={childVariants}
           >
             SOFTWARE ENGINEERING CONFERENCE
           </motion.h2>
           <motion.h1
             className="text-shadow text-center text-[40px] font-bold"
-            transition={transitionConfig(0.4)}
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
+            variants={childVariants}
           >
             EAST CANADA - JANUARY 2025
           </motion.h1>
-        </motion.div>
-        <motion.div
-          className="flex justify-center mt-5"
-          transition={transitionConfig(0.5)}
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-        >
-          <button className="w-[225px] h-[44px] bg-[#FEFEFE] text-font main-button">
-            BUY TICKETS
-          </button>
+          <motion.div
+            className="flex justify-center mt-5"
+            variants={childVariants}
+          >
+            <motion.button
+              className="w-[225px] h-[44px] bg-[#FEFEFE] text-font main-button"
+              variants={childVariants}
+            >
+              BUY TICKETS
+            </motion.button>
+          </motion.div>
         </motion.div>
       </motion.div>
     </>
