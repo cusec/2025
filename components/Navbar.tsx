92a41fbd6a2f1dbb5028547820380cdbdbe7aaaa
TyrellHaywood <tyrelltman@gmail.com>
2024-10-14 19:27:54 -0400

    fix: added logo to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,8 +7,17 @@ import Hamburger from "./Hamburger";
 export default function Navbar() {
   return (
     <nav
-      className={`bg-gray-600/[.40] h-[65px] w-full fixed top-0 z-50 flex justify-end items-center`}
+      className={`bg-gray-600/[.40] h-[65px] w-full fixed top-0 z-50 flex justify-between items-center`}
     >
+      <Link href="/" className="ml-[15px]">
+        <Image
+          className="w-[42px] h-[42px] max-w-[250px] max-h-[250px]"
+          src="/images/logo-main.svg"
+          alt="2025 logo main"
+          width={1080}
+          height={1080}
+        ></Image>
+      </Link>
       <Hamburger />
     </nav>
   );
