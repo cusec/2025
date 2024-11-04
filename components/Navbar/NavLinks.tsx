c90588e5f7a69711a80be5f2ba5d2dd781e610b7
TyrellHaywood <tyrelltman@gmail.com>
2024-11-04 17:20:51 -0500

    fix: updated nav links container postion, fixed page linking

--- components/Navbar/NavLinks.tsx
+++ components/Navbar/NavLinks.tsx
@@ -14,9 +14,10 @@ const NavLinks = ({ isOpen, setIsOpen }: NavLinksProps) => {
 
   return (
     <>
+      {/* mobile nav */}
       <motion.div
-        className={`bg-gray-600/[.40] md:bg-transparent border-t-white border-t-[1px] md:border-none flex flex-col md:flex-row md:justify-end md:items-center gap-5 w-screen h-screen md:h-[65px] fixed top-0 md:right-6 mt-[65px] md:m-0 pt-10 pl-8 md:p-0 transition-all ease-in-out duration-700 z-50 ${
-          isOpen ? "translate-x-0" : "translate-x-[100%] md:translate-x-0"
+        className={`bg-gray-600/[.40] border-t-white border-t-[1px] flex flex-col gap-5 w-screen h-screen fixed top-0 mt-[65px] pt-10 pl-8 transition-all ease-in-out duration-700 z-50 ${
+          isOpen ? "translate-x-0" : "translate-x-[100%]"
         }`}
       >
         <Link
@@ -30,6 +31,24 @@ const NavLinks = ({ isOpen, setIsOpen }: NavLinksProps) => {
           href={"#faq"}
           className="text-[28px] RobotoText font-semibold text-white"
           onClick={toggleOpen}
+        >
+          FAQ
+        </Link>
+      </motion.div>
+
+      {/* desktop nav */}
+      <motion.div
+        className={`hidden md:flex bg-gray-600/[.40] md:bg-transparent md:flex-row md:justify-end md:items-center gap-5 w-screen md:h-[65px] fixed top-0 md:right-6 mt-[65px] md:m-0 pt-10 pl-8 md:p-0 z-50`}
+      >
+        <Link
+          href={"/#tickets"}
+          className="text-[28px] RobotoText font-semibold text-white"
+        >
+          SIGN UP
+        </Link>
+        <Link
+          href={"/#faq"}
+          className="text-[28px] RobotoText font-semibold text-white"
         >
           FAQ
         </Link>
