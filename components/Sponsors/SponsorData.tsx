5d055437b68befe3a8233c6ec71ecd2b3c1e81ef
TJ Klint <timothyjklint@gmail.com>
2024-11-23 03:46:58 -0500

    chore: added stickerbeaver logo to sponsor data

--- components/Sponsors/SponsorData.tsx
+++ components/Sponsors/SponsorData.tsx
@@ -7,11 +7,11 @@ type SponsorData = {
   };
   
   const sponsors: SponsorData = {
-    gold: ["/images/sponsor-gold1.svg", "/images/sponsor-gold2.svg"],
-    silver: ["/images/sponsor-silver1.svg", "/images/sponsor-silver2.svg", "/images/sponsor-silver3.svg"],
-    bronze: ["/images/sponsor-bronze1.svg", "/images/sponsor-bronze2.svg", "/images/sponsor-bronze3.svg", "/images/sponsor-bronze4.svg"],
-    collaborators: ["/images/sponsor-collab1.svg", "/images/sponsor-collab2.svg"],
-    inkind: ["/images/sponsor-inkind1.svg", "/images/sponsor-inkind2.svg", "/images/sponsor-inkind3.svg"],
+    gold: [],
+    silver: ["/images/rbc.png"],
+    bronze: [],
+    collaborators: ["/images/tailed.png"],
+    inkind: ["/images/wolfram.png", "/images/stickerbeaver.png"],
   };
   
   export default sponsors;
