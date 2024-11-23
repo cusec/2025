6962e751c548f1db26bede7001184953100ba119
TJ Klint <timothyjklint@gmail.com>
2024-11-23 03:06:34 -0500

    fix: added types to sponsor data to adhere to best practices

--- components/Sponsors/SponsorData.tsx
+++ components/Sponsors/SponsorData.tsx
@@ -1,4 +1,11 @@
-const sponsors = {
+type SponsorData = {
+    gold: string[];
+    silver: string[];
+    bronze: string[];
+    inkind: string[];
+};
+  
+const sponsors: SponsorData = {
     gold: ["/images/sponsor-gold1.svg", "/images/sponsor-gold2.svg"],
     silver: ["/images/sponsor-silver1.svg", "/images/sponsor-silver2.svg", "/images/sponsor-silver3.svg"],
     bronze: ["/images/sponsor-bronze1.svg", "/images/sponsor-bronze2.svg", "/images/sponsor-bronze3.svg", "/images/sponsor-bronze4.svg"],
