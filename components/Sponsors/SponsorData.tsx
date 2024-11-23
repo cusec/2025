f13a0afbb85fbf314d9797ba5ff3d779e37e6894
TJ Klint <timothyjklint@gmail.com>
2024-11-23 03:05:57 -0500

    feat: added sponsor data

---
+++ components/Sponsors/SponsorData.tsx
@@ -0,0 +1,9 @@
+const sponsors = {
+    gold: ["/images/sponsor-gold1.svg", "/images/sponsor-gold2.svg"],
+    silver: ["/images/sponsor-silver1.svg", "/images/sponsor-silver2.svg", "/images/sponsor-silver3.svg"],
+    bronze: ["/images/sponsor-bronze1.svg", "/images/sponsor-bronze2.svg", "/images/sponsor-bronze3.svg", "/images/sponsor-bronze4.svg"],
+    inkind: ["/images/sponsor-inkind1.svg", "/images/sponsor-inkind2.svg", "/images/sponsor-inkind3.svg"],
+};
+  
+export default sponsors;
+  
