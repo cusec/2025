fe9fe2eebfbe4ecb85c1750e15184d097666466d
TJ Klint <112092190+tjklint@users.noreply.github.com>
2024-12-15 14:03:08 -0500

    chore: sponsor data now uses a dictionary to hold links & images

--- components/Sponsors/SponsorData.tsx
+++ components/Sponsors/SponsorData.tsx
@@ -1,22 +1,31 @@
-type SponsorData = {
-  gold: string[];
-  silver: string[];
-  bronze: string[];
-  collaborators: string[];
-  inkind: string[];
-};
+import Sponsor from "./Sponsor";
 
+type SponsorData = {
+  gold: Sponsor[];
+  silver: Sponsor[];
+  bronze: Sponsor[];
+  collaborators: Sponsor[];
+  inkind: Sponsor[];
+};
+    
 const sponsors: SponsorData = {
-  gold: ["/images/sponsors/rbc.png"],
-  silver: [
-    "/images/sponsors/Compulsion_Games.png",
-    "/images/sponsors/fellow.webp",
+  gold: [
+    { image: "/images/sponsors/rbc.png", link: "https://www.rbc.com/about-rbc.html"}
   ],
-  bronze: ["/images/sponsors/ciena.png", "/images/sponsors/cse.svg"],
-  collaborators: ["/images/sponsors/tailed.png"],
+  silver: [
+    { image: "/images/sponsors/Compulsion_Games.png", link: "https://compulsiongames.com/" },
+    { image: "/images/sponsors/fellow.webp", link: "https://fellow.app/" }
+  ],
+  bronze: [
+    { image: "/images/sponsors/ciena.png", link: "https://www.ciena.com" },
+    { image: "/images/sponsors/cse.svg", link: "https://www.cse-cst.gc.ca/" }
+  ],
+  collaborators: [
+    { image: "/images/tailed.png", link: "https://www.tailed.com" }
+  ],
   inkind: [
-    "/images/sponsors/wolfram.png",
-    "/images/sponsors/stickerbeaver.png",
+    { image: "/images/sponsors/wolfram.png", link: "https://www.wolframalpha.com/" },
+    { image: "/images/sponsors/stickerbeaver.png", link: "https://www.stickerbeaver.com" }
   ],
 };
 
