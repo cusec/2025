81a5f0a4eab817812341cb47aa11f451005974d6
Tyrell H <73722828+TyrellHaywood@users.noreply.github.com>
2024-12-30 13:03:52 -0600

    fix: updated speakers type defining

--- components/Speakers/Speakers.tsx
+++ components/Speakers/Speakers.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import speakers from './speakers.json';
-import SpeakerCard from './SpeakerCard';
-import KeynoteSpeakerCard from './KeynoteSpeakerCard';
+import React from "react";
+import speakersData from "./speakers.json";
+import Speaker from "./Speaker";
+import SpeakerCard from "./SpeakerCard";
+import KeynoteSpeakerCard from "./KeynoteSpeakerCard";
+
+const speakers: Speaker[] = speakersData as Speaker[];
 
 const Speakers = () => {
-
-  const keynotes = speakers.filter((speaker) => speaker.type === 'keynote');
-  const remainingSpeakers = speakers.filter((speaker) => speaker.type !== 'keynote');
+  const keynotes = speakers.filter((speaker) => speaker.type === "keynote");
+  const remainingSpeakers = speakers.filter(
+    (speaker) => speaker.type !== "keynote"
+  );
 
   return (
     <div className="container mx-auto p-4">
       <h2 className="text-5xl md:text-7xl font-bold whiteText mb-8">
         OUR SPEAKERS
-      </h2>      
+      </h2>
       {keynotes.map((speaker) => (
         <KeynoteSpeakerCard key={speaker.name} speaker={speaker} />
       ))}
